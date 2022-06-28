c39362541ca60db67db8655910ce0b71141a55d0
zhaomin <min.zhao@newsbreak.com>
2022-06-28 23:50:14 +0800

    commit by ThreadPoolExecutor

--- run_get_recall_result.sh
+++ run_get_recall_result.sh
@@ -14,18 +14,18 @@ function cleanup() {
 
 while true; do
     start=$(TZ=UTC-8 date +%s)
-    five_mins_later=$(TZ=UTC-8 date -d '5 mins' +%s)
-    python ${SCRIPT_ROOT_DIR}/fetch_requests.py --limit 100000 --start $(TZ=UTC-8 date -d '-5 mins' +%s) > ${SCRIPT_ROOT_DIR}/${RECALL_RESULT_FOLDER}/${start}_uid_ts.txt
+    one_mins_later=$(TZ=UTC-8 date -d '1 mins' +%s)
+    python ${SCRIPT_ROOT_DIR}/fetch_requests.py --limit 100000 --start $(TZ=UTC-8 date -d '-1 mins' +%s) > ${SCRIPT_ROOT_DIR}/${RECALL_RESULT_FOLDER}/${start}_uid_ts.txt
     end_fetch=$(TZ=UTC-8 date +%s)
     cat ${SCRIPT_ROOT_DIR}/${RECALL_RESULT_FOLDER}/${start}_uid_ts.txt | python ${SCRIPT_ROOT_DIR}/call_recall.py --url ${RECALL_URL} > ${SCRIPT_ROOT_DIR}/${RECALL_RESULT_FOLDER}/${start}_recall_result.txt
     end_call=$(TZ=UTC-8 date +%s)
     echo "Now time is $(TZ=UTC-8 date '+%Y-%m-%d %H:%M:%S'), fetch_requests cost time is $((end_fetch-start))s, call_recall cost time is $((end_call-end_fetch))s"
     while true; do
-      if [[ $(TZ=UTC-8 date +%s) -gt five_mins_later ]]; then
+      if [[ $(TZ=UTC-8 date +%s) -gt one_mins_later ]]; then
         echo "We have waited about $(($(TZ=UTC-8 date +%s) - end_call))s. Starting the next round."
         break
       else
-        sleep 30s
+        sleep 5s
       fi
     done
     echo -e "\n"
